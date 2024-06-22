a53f836452a3e24bd59499a949e64c0c624976a3
Юра Войтюк <vynograd@proton.me>
2024-06-22 02:02:13 +0000

    add vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from "next/app";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import type { ReactElement } from "react";
 import "../style.css";
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
     <>
-      <SpeedInsights />
       <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
     </>
   );
 }
